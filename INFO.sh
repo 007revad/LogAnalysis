591fabd0740d3df0dbf1e10c3c5ce33c12c39b4b
Dave Russell <39733752+007revad@users.noreply.github.com>
2026-02-16 08:27:47 +1100

    Update INFO.sh with new distributor URL and help URL

--- INFO.sh
+++ INFO.sh
@@ -20,9 +20,10 @@ package_icon="PACKAGE_ICON.PNG"
 package_icon_256="PACKAGE_ICON_256.PNG"
 dsmappname="SYNO.SDS.LogAnalysis.Application"
 maintainer="Tommes"
+distributor_url="https://github.com/toafez/LogAnalysis/releases"
 description="The Linux system on which the DSM is based logs all events, problems and errors of the system and running services in so-called log files. This information is stored in various text files, which are usually located in the /var/log folder and in connected subfolders. The viewing of these text files is normally done in any editor, which is executed via the terminal. Within Synology DiskStation Manager (DSM), these files can now be conveniently viewed and searched using the GUI of LogAnalysis."
 description_enu="The Linux system on which the DSM is based logs all events, problems and errors of the system and running services in so-called log files. This information is stored in various text files, which are usually located in the /var/log folder and in connected subfolders. The viewing of these text files is normally done in any editor, which is executed via the terminal. Within Synology DiskStation Manager (DSM), these files can now be conveniently viewed and searched using the GUI of LogAnalysis."
 description_ger="Das dem DSM zugrunde liegende Linux-System protokolliert in sogenannten Protokoll- oder auch Logdateien (engl. logfiles) sämtliche Ereignisse, Probleme und Fehler des Systems sowie laufender Dienste. Abgelegt werden diese Informationen hierbei in unterschiedlichen Textdateien, welche sich in der Regel im Ordner /var/log sowie angeschlossenen Unterordnern befinden. Das Betrachten dieser Textdateien erfolgt normalerweise in einem beliebigen Editor, welcher über das Terminal ausgeführt wird. Innerhalb des Synology DiskStation Managers (DSM) können diese Dateien nun komfortabel über die GUI von LogAnalysis betrachtet und durchsucht werden."
-helpurl="https://www.synology-forum.de/showthread.html?107180-LogAnalysis-GUI-zum-betrachten-und-durchsuchen-von-var-log"
+helpurl="https://www.synology-forum.de/threads/LogAnalysis-GUI-zum-betrachten-und-durchsuchen-von-var-log.107180"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
